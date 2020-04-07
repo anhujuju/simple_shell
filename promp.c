ddfe4055acc9f28adc59171c81b531ecda6c5589
Andres <anhujuju1gmai.com>
2020-04-07 21:52:45 +0000

    promp

---
+++ promp.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _promp - Behavior of signals
+ * @error: Error ID from signal
+**/
+
+void _promp(int error)
+
+{
+switch (error)
+{
+case :
+    write(1, "\n", 1);
+    write(STDOUT_FILENO, "$ ", 2);
+    break;
+
+default:
+   write(2, "Issued command \n", 15);
+    break;
+}
+
+}
+
+
