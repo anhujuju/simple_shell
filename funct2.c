45977d80c4f93906b305ec6cb27c909a306173a3
JuanJoseGomezR <juangomez0149@gmail.com>
2020-04-08 17:33:35 +0000

    [Fixed] betty

---
+++ funct2.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+  * _strspn - gets length of a prefix substring
+  * @s: string
+  * @delimiter: delimiter
+  * Return: Counter
+  */
+unsigned int _strspn(char *s, char *delimiter)
+{
+	unsigned int i, j;
+	unsigned int count;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; s++; i++)
+	{
+		for (j = 0; delimiter[j] != '\0'; j++)
+		{
+			if (*s == delimiter[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (count < i)
+			break;
+	}
+	return (count);
+}
+/**
+  *
+  *
+  *
+  */
+char *_strtok(char *s, char *delimiter);
--- promp.c
+++ promp.c
@@ -3,22 +3,22 @@
 /**
  * _promp - Behavior of signals
  * @error: Error ID from signal
-**/
+ **/
 
 void _promp(int error)
 
 {
-switch (error)
-{
-case :
-    write(1, "\n", 1);
-    write(STDOUT_FILENO, "$ ", 2);
-    break;
+	switch (error)
+	{
+case:
+	write(1, "\n", 1);
+	write(STDOUT_FILENO, "$ ", 2);
+	break;
 
 default:
-   write(2, "Issued command \n", 15);
-    break;
-}
+	write(2, "Issued command \n", 15);
+	break;
+	}
 
 }
 
