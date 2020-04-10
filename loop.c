0e0de5f363b598a0c2c797e64b4d75a1f98cf23e
Andres <anhujuju1gmai.com>
2020-04-10 17:57:29 +0000

    agree loop

--- loop.c
+++ loop.c
@@ -1,20 +1,46 @@
 #include "holberton.h"
-/**
-  * loop - read commds passd by user
-  * Return: nothing
-  */
-void loop(void)
-{
-	char *line;
-	char **args;
-	int status;
-	/**
-	 * read the commands entered by the usar
-	 * it will parse the commands computer can understand the commands
-	 * execute the commands
-	 **/
-	do {
-		write(STDOUT_FILENO, "$ ", 2);
-	} while (status);
 
+int loop(void)
+{
+ char *line;
+ char **args;
+ int status, j;   
+ /**
+  * read the commands entered by the usar
+  * it will parse the commands computer can understand the commands
+  * execute the commands
+  * */
+ 
+ do
+ {
+    j = 0;
+    write(STDOUT_FILENO, "$ ", 2);
+    if (line == NULL)
+    {
+       break;
+    }
+    args = _splitline(line);
+    if (args[0] != NULL)
+      {
+         status = execute(args, &j, 0);
+
+      }
+   if (status == 0)
+   {
+      free (line);
+   } 
+   if (j == 1)
+   {
+      free (args[0]);
+   }
+   else
+   {
+      if (status != 0)
+      {
+         free(line);
+      }
+      free (args);
+   }
+ } while (status);
+ 
 }
