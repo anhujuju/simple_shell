5e4876af094529fefae77297464b92e8fc4b2990
Andres <anhujuju1gmai.com>
2020-04-14 23:32:49 +0000

    change

--- holberton.h
+++ holberton.h
@@ -30,7 +30,6 @@ unsigned int _strspn(char *s, char *delimiter);
 void No_interactive(char *line, char **args);
 char *_strchr(char *s, char b);
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-char *_strtok_r(char *s, const char *delimiter, char **save_str);
 char *_strtok(char *s, char *delimiter);
 
 #endif
